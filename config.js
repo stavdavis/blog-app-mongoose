b87a2c2400e0670b2b5b4bca0e071d19087857fb
Stav Davis <stav.davis@gmail.com>
2017-11-28 22:19:41 -0800

    corrected error in DATABASE_URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+                      'mongodb://localhost/blog-app-mongoose';
 exports.PORT = process.env.PORT || 8080;
