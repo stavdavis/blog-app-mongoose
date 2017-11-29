aefae72d1052e3cb98fe9d4973c21f74d823693c
Stav Davis <stav.davis@gmail.com>
2017-11-28 23:45:19 -0800

    corrected database url again

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app-mongoose';
+                      'mongodb://stav.davis:saltotBaavir22!@ds123976.mlab.com:23976/blog-app-mongoose';
 exports.PORT = process.env.PORT || 8080;
